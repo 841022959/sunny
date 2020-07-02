cfcc234226d4183d35518336b6e5184671f7bb71
841022959 <67259404+841022959@users.noreply.github.com>
2020-07-02 18:07:20 +0800

    First commit.

---
+++ app/src/main/java/cap/java/cj841/MainActivity.java
@@ -0,0 +1,25 @@
+package cap.java.cj841;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //缓存数据
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //缓存数据刷新
+        if (prefs.getString("weather",null) != null){
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
